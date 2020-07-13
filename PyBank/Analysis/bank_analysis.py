c0dfd2b51062283834f55465f7ede0b32ce17f80
kellyshull <kelly.t.shull@gmail.com>
2020-07-12 23:14:38 -0700

    modified:   bank_analysis.py

--- PyBank/Analysis/bank_analysis.py
+++ PyBank/Analysis/bank_analysis.py
@@ -1,3 +1,4 @@
+#dependencies
 import os
 import csv
 
@@ -6,15 +7,30 @@ import csv
 bank_data = os.path.join("..","Resources", "budget_data.csv")
 
 
-
-
 # put with open statement here and skip your header using next()
 with open(bank_data, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
     print(f'Header: {csv_header}')
-    
 
-#Print Statements
-print("Financial Analysis")
-print("..........................")
+
+    #put your lists here 
+    months = []
+    profit_loss = []
+    change_monthly = []
+
+    #variables
+    row_count = 0
+    total_net = 0
+    total_change = 0
+
+
+    # append your rows for profit and months
+    for rows in csvreader:
+        months.append(rows[0])
+        profit_loss.append(rows[1])
+        
+        row_count = row_count + 1
+        
+
+   
